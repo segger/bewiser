e8c10b78f7872fb6a389bb0e7b1a34da623d22b2
segger <johanna.lynn.lahti@gmail.com>
2020-01-02 19:35:01 +0100

    Add gradient color for practice list view

--- lib/models/practice.dart
+++ lib/models/practice.dart
@@ -1,5 +1,7 @@
 class Practice {
   final int id;
+  final int gradientColor;
+  final int color;
   final String title;
-  const Practice(this.id, this.title);
+  const Practice(this.id, this.gradientColor, this.color, this.title);
 }
--- lib/pages/practice_page.dart
+++ lib/pages/practice_page.dart
@@ -15,13 +15,33 @@ class _PracticePageState extends State<PracticePage> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: Constants.of(context).practices.map((practice) => ListTile(
-        title: Text(practice.title),
-        onTap: () => Navigator.push(context, MaterialPageRoute(
-          builder: (context) => PracticeView(practice),
-        )),
-      )).toList(),
+    return Container(
+      color: Colors.amber[50],
+      child: Container(
+        margin: EdgeInsets.only(left: 15, right: 15, top: 15),
+        color: Colors.amber[500],
+        child: ListView(
+          children: Constants.of(context).practices.map((practice) =>
+          Container(
+            color: Colors.amber[practice.gradientColor],
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.amber[practice.color],
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                )
+              ),
+              child: ListTile(
+                title: Text(practice.title),
+                onTap: () => Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => PracticeView(practice),
+                )),
+              )
+            ),
+          )).toList(),
+        ),
+      ),
     );
   }
 }
