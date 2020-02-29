991eef21e9b4167c7fe683889523780bfe0c2a77
segger <johanna.lynn.lahti@gmail.com>
2020-02-29 18:47:04 +0100

    Add custom drawer to statistics page

--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class StatisticsPage extends StatefulWidget {
@@ -8,10 +11,66 @@ class StatisticsPage extends StatefulWidget {
 }
 
 class _StatisticsPageState extends State<StatisticsPage> {
+  List<DrawingPoints> points = List();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('StatisticsPage'),
+    return Scaffold(
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+              points: renderBox.globalToLocal(details.globalPosition),
+              paint: Paint()
+                ..strokeCap = StrokeCap.butt
+                ..isAntiAlias = true
+                ..color = Colors.black
+                ..strokeWidth = 3.0));
+          });
+        },
+        onPanEnd: (details) {
+          setState(() {
+            points.add(null);
+          });
+        },
+        child: CustomPaint(
+          size: Size.infinite,
+          painter: DrawingPainter(
+            pointsList: points,
+          ),
+        )
+      ),
     );
   }
+}
+
+class DrawingPainter extends CustomPainter {
+  DrawingPainter({this.pointsList});
+  List<DrawingPoints> pointsList;
+  List<Offset> offsetPoints = List();
+
+  @override 
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < pointsList.length - 1; i++) {
+      if (pointsList[i] != null && pointsList[i+1] != null) {
+        canvas.drawLine(pointsList[i].points, pointsList[i+1].points, pointsList[i].paint);
+      } else if (pointsList[i] != null && pointsList[i+1] == null) {
+        offsetPoints.clear();
+        offsetPoints.add(pointsList[i].points);
+        offsetPoints.add(Offset(
+          pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
+        canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+}
+
+class DrawingPoints {
+  Paint paint;
+  Offset points;
+  DrawingPoints({this.points, this.paint});
 }
